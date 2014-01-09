f026b339fcb433209a677275dfce543a4c855ac9
Vincent Batoufflet <vincent@batoufflet.info>
2014-01-09 07:38:13 +0100

    Make near-zero values zero instad of null.

--- src/facette/common/types.go
+++ src/facette/common/types.go
@@ -10,9 +10,11 @@ type PlotValue float64
 
 // MarshalJSON handles JSON marshaling of the PlotValue type.
 func (value PlotValue) MarshalJSON() ([]byte, error) {
-	// Make NaN and very small values null
-	if math.IsNaN(float64(value)) || math.Exp(float64(value)) == 1 {
+	// Handle NaN and near-zero values
+	if math.IsNaN(float64(value)) {
 		return json.Marshal(nil)
+	} else if math.Exp(float64(value)) == 1 {
+		return json.Marshal(0)
 	}
 
 	return json.Marshal(float64(value))
