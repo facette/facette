7824f9c93c9df4626829fbfcd74fad51ddb0d572
Marc Falzon <marc@falzon.me>
2014-05-29 17:30:28 +0200

    Add config.Get* helper functions comments

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -106,21 +106,25 @@ func getSetting(config map[string]interface{}, setting string, kind reflect.Kind
 	return config[setting], nil
 }
 
+// GetString returns the string value of a configuration setting.
 func GetString(config map[string]interface{}, setting string, mandatory bool) (string, error) {
 	value, err := getSetting(config, setting, reflect.String, mandatory, "")
 	return value.(string), err
 }
 
+// GetInt returns the int value of a configuration setting.
 func GetInt(config map[string]interface{}, setting string, mandatory bool) (int, error) {
 	value, err := getSetting(config, setting, reflect.Int, mandatory, 0)
 	return value.(int), err
 }
 
+// GetFloat returns the float value of a configuration setting.
 func GetFloat(config map[string]interface{}, setting string, mandatory bool) (float64, error) {
 	value, err := getSetting(config, setting, reflect.Float64, mandatory, 0.0)
 	return value.(float64), err
 }
 
+// GetBool returns the bool value of a configuration setting.
 func GetBool(config map[string]interface{}, setting string, mandatory bool) (bool, error) {
 	value, err := getSetting(config, setting, reflect.Bool, mandatory, false)
 	return value.(bool), err
