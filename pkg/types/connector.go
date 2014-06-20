e2a3e1adda58673d444ecd6457977fcc87a7de10
Marc Falzon <marc@falzon.me>
2014-06-20 16:56:34 +0200

    Add `String()` method to types/connector structs

--- pkg/types/connector.go
+++ pkg/types/connector.go
@@ -1,6 +1,10 @@
 package types
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 // PlotQuery represents a connector plot query.
 type PlotQuery struct {
@@ -11,11 +15,12 @@ type PlotQuery struct {
 	Percentiles []float64
 }
 
-// MetricQuery represents a metric entry in a SerieQuery.
-type MetricQuery struct {
+// GroupQuery represents a plot group query.
+type GroupQuery struct {
 	Name   string
-	Origin string
-	Source string
+	Type   int
+	Series []*SerieQuery
+	Scale  float64
 }
 
 // SerieQuery represents a serie entry in a GroupQuery.
@@ -25,16 +30,70 @@ type SerieQuery struct {
 	Scale  float64
 }
 
-// GroupQuery represents a plot group query.
-type GroupQuery struct {
+// MetricQuery represents a metric entry in a SerieQuery.
+type MetricQuery struct {
 	Name   string
-	Type   int
-	Series []*SerieQuery
-	Scale  float64
+	Origin string
+	Source string
 }
 
 // PlotResult represents a plot request result.
 type PlotResult struct {
 	Plots []PlotValue
 	Info  map[string]PlotValue
+}
+
+func (plotQuery *PlotQuery) String() string {
+	return fmt.Sprintf(
+		"PlotQuery{StartTime:%s EndTime:%s Step:%s Percentiles:[%s] Group:%s}",
+		plotQuery.StartTime.String(),
+		plotQuery.EndTime.String(),
+		plotQuery.Step.String(),
+		func(percentiles []float64) string {
+			percentilesStrings := make([]string, len(percentiles))
+
+			for i, percentile := range percentiles {
+				percentilesStrings[i] = fmt.Sprintf("%s", percentile)
+			}
+
+			return strings.Join(percentilesStrings, ", ")
+		}(plotQuery.Percentiles),
+		plotQuery.Group,
+	)
+}
+
+func (groupQuery *GroupQuery) String() string {
+	return fmt.Sprintf(
+		"GroupQuery{Name:\"%s\" Type:%d Scale:%f Series:[%s]}",
+		groupQuery.Name,
+		groupQuery.Type,
+		groupQuery.Scale,
+		func(series []*SerieQuery) string {
+			seriesStrings := make([]string, len(series))
+
+			for i, serie := range series {
+				seriesStrings[i] = fmt.Sprintf("%s", serie)
+			}
+
+			return strings.Join(seriesStrings, ", ")
+		}(groupQuery.Series),
+	)
+}
+
+func (serieQuery *SerieQuery) String() string {
+	return fmt.Sprintf(
+		"SerieQuery{Name:\"%s\" Scale:%f Metric:%s}",
+		serieQuery.Name,
+		serieQuery.Scale,
+		serieQuery.Metric,
+	)
+}
+
+func (metricQuery *MetricQuery) String() string {
+	return fmt.Sprintf(
+		"MetricQuery{Name:\"%s\" Source:\"%s\" Origin:\"%s\"}",
+		metricQuery.Name,
+		metricQuery.Source,
+		metricQuery.Origin,
+	)
 }
