7dbca17e1109a5a514b7b7d95431a66700be618b
Marc Falzon <marc@falzon.me>
2014-07-27 16:03:43 +0200

    Add function plot.AvgSeries()

--- pkg/plot/func.go
+++ pkg/plot/func.go
@@ -4,6 +4,52 @@ import (
 	"fmt"
 )
 
+// AvgSeries returns a new series averaging each series' datapoints.
+func AvgSeries(seriesList []Series) (Series, error) {
+	nSeries := len(seriesList)
+
+	if nSeries == 0 {
+		return Series{}, fmt.Errorf("no series provided")
+	}
+
+	// Find out the longest series of the list
+	maxPlots := len(seriesList[0].Plots)
+	for i := range seriesList {
+		if len(seriesList[i].Plots) > maxPlots {
+			maxPlots = len(seriesList[i].Plots)
+		}
+	}
+
+	avgSeries := Series{
+		Plots:   make([]Plot, maxPlots),
+		Summary: make(map[string]Value),
+	}
+
+	for plotIndex := 0; plotIndex < maxPlots; plotIndex++ {
+		var validPlots Value
+
+		for _, series := range seriesList {
+			// Skip shorter series
+			if plotIndex >= len(series.Plots) {
+				continue
+			}
+
+			if !series.Plots[plotIndex].Value.IsNaN() {
+				avgSeries.Plots[plotIndex].Value += series.Plots[plotIndex].Value
+				avgSeries.Plots[plotIndex].Time = series.Plots[plotIndex].Time
+				validPlots++
+			}
+
+		}
+
+		if validPlots > 0 {
+			avgSeries.Plots[plotIndex].Value /= validPlots
+		}
+	}
+
+	return avgSeries, nil
+}
+
 // SumSeries add series plots together and return the sum at each datapoint.
 func SumSeries(seriesList []Series) (Series, error) {
 	nSeries := len(seriesList)
--- pkg/plot/func_test.go
+++ pkg/plot/func_test.go
@@ -6,6 +6,90 @@ import (
 	"testing"
 )
 
+func Test_FuncAvgSeries(test *testing.T) {
+	var (
+		// Valid series
+		testFull = []Series{
+			{Plots: []Plot{{Value: 61}, {Value: 69}, {Value: 98}, {Value: 56}, {Value: 43}}},
+			{Plots: []Plot{{Value: 68}, {Value: 87}, {Value: 95}, {Value: 69}, {Value: 79}}},
+			{Plots: []Plot{{Value: 99}, {Value: 54}, {Value: 88}, {Value: 99}, {Value: 77}}},
+			{Plots: []Plot{{Value: 85}, {Value: 62}, {Value: 71}, {Value: 78}, {Value: 72}}},
+			{Plots: []Plot{{Value: 89}, {Value: 70}, {Value: 96}, {Value: 93}, {Value: 66}}},
+		}
+
+		expectedFull = Series{
+			Plots: []Plot{{Value: 80.4}, {Value: 68.4}, {Value: 89.6}, {Value: 79}, {Value: 67.4}},
+		}
+
+		// Valid series featuring NaN plot values
+		testNaN = []Series{
+			{Plots: []Plot{
+				{Value: 61}, {Value: 69}, {Value: 98}, {Value: 56}, {Value: 43}},
+			},
+			{Plots: []Plot{
+				{Value: Value(math.NaN())}, {Value: 62}, {Value: 71}, {Value: 78}, {Value: 72}},
+			},
+			{Plots: []Plot{
+				{Value: 89}, {Value: 70}, {Value: Value(math.NaN())}, {Value: 93}, {Value: 66}},
+			},
+		}
+
+		expectedNaN = Series{
+			Plots: []Plot{
+				{Value: 75}, {Value: 67}, {Value: 84.5},
+				{Value: 75.66666666666667}, {Value: 60.333333333333336}},
+		}
+
+		// Valid series: not normalized
+		testNotNormalized = []Series{
+			Series{Plots: []Plot{{Value: 85}, {Value: 62}, {Value: 71}, {Value: 78}, {Value: 72}}},
+			Series{Plots: []Plot{{Value: 70}, {Value: 96}, {Value: 93}}},
+			Series{Plots: []Plot{{Value: 55}, {Value: 48}, {Value: 39}, {Value: 53}}},
+		}
+
+		expectedNotNormalized = Series{
+			Plots: []Plot{
+				{Value: 70}, {Value: 68.66666666666667}, {Value: 67.66666666666667}, {Value: 65.5}, {Value: 72}},
+		}
+	)
+
+	avgFull, err := AvgSeries(testFull)
+	if err != nil {
+		test.Logf("AvgSeries(testFull) returned an error: %s", err)
+		test.Fail()
+	}
+
+	if err = compareSeries(expectedFull, avgFull); err != nil {
+		test.Logf(fmt.Sprintf("AvgSeries(testFull): %s", err))
+		test.Fail()
+		return
+	}
+
+	avgNaN, err := AvgSeries(testNaN)
+	if err != nil {
+		test.Logf("AvgSeries(testNaN) returned an error: %s", err)
+		test.Fail()
+	}
+
+	if err = compareSeries(expectedNaN, avgNaN); err != nil {
+		test.Logf(fmt.Sprintf("AvgSeries(testNaN): %s", err))
+		test.Fail()
+		return
+	}
+
+	avgNotNormalized, err := AvgSeries(testNotNormalized)
+	if err != nil {
+		test.Logf("AvgSeries(testNotNormalized) returned an error: %s", err)
+		test.Fail()
+	}
+
+	if err = compareSeries(expectedNotNormalized, avgNotNormalized); err != nil {
+		test.Logf(fmt.Sprintf("AvgSeries(testNotNormalized): %s", err))
+		test.Fail()
+		return
+	}
+}
+
 func Test_FuncSumSeries(test *testing.T) {
 	var (
 		// Valid series
