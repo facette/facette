060fb5231976a06dfebface2905aa360584b0edd
Vincent Batoufflet <vincent@batoufflet.info>
2014-08-23 18:35:48 +0200

    Remove unused `ADMIN_REFRESH_TIMEOUT' JS variable

--- cmd/facette/js/admin/intro.js
+++ cmd/facette/js/admin/intro.js
@@ -1,6 +1,5 @@
 
 /* Admin */
 
-var ADMIN_PANES           = {},
-    ADMIN_FIELD_TIMEOUT   = null,
-    ADMIN_REFRESH_TIMEOUT = null;
+var ADMIN_PANES         = {},
+    ADMIN_FIELD_TIMEOUT = null;
