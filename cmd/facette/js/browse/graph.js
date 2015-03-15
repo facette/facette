52c83f621d763f2b4d99273cd3f63e2719929947
Vincent Batoufflet <vincent@batoufflet.info>
2015-03-15 09:26:14 +0100

    Add single graph edition button in browse pane

--- cmd/facette/js/browse/graph.js
+++ cmd/facette/js/browse/graph.js
@@ -1,5 +1,10 @@
 
 function browseGraphSetupTerminate() {
     // Register links
+    linkRegister('edit-graph', function (e) {
+        // Go to Administration Panel
+        window.location = urlPrefix + '/admin/graphs/' + $(e.target).closest('[data-pane]').opts('pane').id;
+    });
+
     linkRegister('set-refresh', browseSetRefresh);
 }
