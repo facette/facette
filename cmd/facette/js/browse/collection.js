4e04cb50c113fa47a00f301dab45a6c031af9b74
Vincent Batoufflet <vincent@batoufflet.info>
2015-05-22 08:26:56 +0200

    Add tree state save and restore support

--- cmd/facette/js/browse/collection.js
+++ cmd/facette/js/browse/collection.js
@@ -10,11 +10,56 @@ function browseCollectionHandleTree(e) {
             .children('.treecntr').toggle();
     }
 
+    // Save new tree state
+    browseCollectionSaveTreeState();
+
     e.preventDefault();
     e.stopPropagation();
 }
 
+function browseCollectionResoreTreeState() {
+    var state;
+
+    if (!localStorage)
+        return;
+
+    try {
+        state = JSON.parse(localStorage.getItem('collections-tree'));
+
+        // Toggle previously unfolded tree items
+        $('[data-tree=collections] [data-treeitem]').each(function () {
+            var $item = $(this);
+
+            if (!state[$item.attr('data-treeitem')])
+                return;
+
+            $item.toggleClass('folded unfolded')
+                .children('.treecntr').toggle();
+        });
+    } catch (e) {}
+}
+
+function browseCollectionSaveTreeState() {
+    var state;
+
+    if (!localStorage)
+        return;
+
+    // Save tree items states
+    state = {};
+
+    $('[data-tree=collections] [data-treeitem]').each(function () {
+        var $item = $(this);
+        state[$item.attr('data-treeitem')] = $item.hasClass('unfolded');
+    });
+
+    localStorage.setItem('collections-tree', JSON.stringify(state));
+}
+
 function browseCollectionSetupTerminateTree() {
+    // Restore saved tree state
+    browseCollectionResoreTreeState();
+
     // Attach events
     $body.on('click', '[data-tree=collections] a', browseCollectionHandleTree);
 }
