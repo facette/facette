76c6be8fc2ff1579a33e1813c253d102ca3a932f
Vincent Batoufflet <vincent@batoufflet.info>
2015-05-22 08:26:23 +0200

    Fix missing collection tree event handler

--- cmd/facette/js/browse/collection.js
+++ cmd/facette/js/browse/collection.js
@@ -1,4 +1,24 @@
 
+function browseCollectionHandleTree(e) {
+    var $target = $(e.target),
+        $item = $target.closest('[data-treeitem]');
+
+    if ($target.closest('.icon').length === 0 || !$item.hasClass('folded') && !$item.hasClass('unfolded')) {
+        window.location = urlPrefix + '/browse/collections/' + $item.attr('data-treeitem');
+    } else {
+        $item.toggleClass('folded unfolded')
+            .children('.treecntr').toggle();
+    }
+
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+function browseCollectionSetupTerminateTree() {
+    // Attach events
+    $body.on('click', '[data-tree=collections] a', browseCollectionHandleTree);
+}
+
 function browseCollectionSetupTerminate() {
     $('[data-treeitem=' + paneMatch('collection-show').opts('pane').id + ']')
         .addClass('current')
@@ -17,22 +37,4 @@ function browseCollectionSetupTerminate() {
     linkRegister('set-global-refresh', browseSetRefresh);
 
     linkRegister('toggle-legends', browseToggleLegend);
-
-    // Attach events
-    $body
-        .on('click', '[data-tree=collections] a', function (e) {
-            var $target = $(e.target),
-                $item = $target.closest('[data-treeitem]');
-
-            if ($target.closest('.icon').length === 0 || !$item.hasClass('folded') && !$item.hasClass('unfolded')) {
-                window.location = urlPrefix + '/browse/collections/' + $item.attr('data-treeitem');
-            } else {
-                $item.children('.treecntr')
-                    .toggle()
-                    .toggleClass('folded unfolded');
-            }
-
-            e.preventDefault();
-            e.stopPropagation();
-        });
 }
