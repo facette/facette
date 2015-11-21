c37c4a2be6765cde02cee1b16a50c80c963d26be
Vincent Batoufflet <vincent@batoufflet.info>
2015-11-21 09:13:49 +0100

    Simplify group expansion code

--- pkg/library/group.go
+++ pkg/library/group.go
@@ -1,23 +1,13 @@
 package library
 
 import (
-	"path"
-	"regexp"
-	"sort"
-	"strings"
-	"sync"
-
 	"github.com/facette/facette/pkg/logger"
-	"github.com/fatih/set"
+	"github.com/facette/facette/pkg/utils"
 )
 
 const (
 	// LibraryGroupPrefix represents the prefix for sources and metrics groups names.
 	LibraryGroupPrefix = "group:"
-	// LibraryMatchPrefixGlob represents the prefix for glob matching patterns.
-	LibraryMatchPrefixGlob = "glob:"
-	// LibraryMatchPrefixRegexp represents the prefix for regexp matching patterns.
-	LibraryMatchPrefixRegexp = "regexp:"
 )
 
 // Group represents a source or metric group.
@@ -50,40 +40,9 @@ func (library *Library) expandGroup(name string, groupType int, sourceName strin
 		return []string{}
 	}
 
-	// Launch expansion goroutine
-	itemSet := set.New(set.ThreadSafe)
-	itemChan := make(chan [2]string)
-	itemWg := &sync.WaitGroup{}
-
-	go func(itemSet set.Interface, itemChan chan [2]string, itemWg *sync.WaitGroup) {
-		var re *regexp.Regexp
-
-		itemWg.Add(1)
-
-		for entry := range itemChan {
-
-			if strings.HasPrefix(entry[0], LibraryMatchPrefixGlob) {
-				if ok, _ := path.Match(strings.TrimPrefix(entry[0], LibraryMatchPrefixGlob), entry[1]); !ok {
-					continue
-				}
-			} else if strings.HasPrefix(entry[0], LibraryMatchPrefixRegexp) {
-				re = regexp.MustCompile(strings.TrimPrefix(entry[0], LibraryMatchPrefixRegexp))
-
-				if !re.MatchString(entry[1]) {
-					continue
-				}
-			} else if entry[0] != entry[1] {
-				continue
-			}
-
-			itemSet.Add(entry[1])
-		}
-
-		itemWg.Done()
-	}(itemSet, itemChan, itemWg)
-
 	// Parse group entries for patterns
 	group := item.(*Group)
+	result := []string{}
 
 	for _, entry := range group.Entries {
 		if groupType == LibraryItemSourceGroup {
@@ -94,7 +53,9 @@ func (library *Library) expandGroup(name string, groupType int, sourceName strin
 			}
 
 			for _, source := range origin.GetSources() {
-				itemChan <- [2]string{entry.Pattern, source.Name}
+				if utils.FilterMatch(entry.Pattern, source.Name) {
+					result = append(result, source.Name)
+				}
 			}
 		} else {
 			source, err := library.Catalog.GetSource(entry.Origin, sourceName)
@@ -104,17 +65,12 @@ func (library *Library) expandGroup(name string, groupType int, sourceName strin
 			}
 
 			for _, metric := range source.GetMetrics() {
-				itemChan <- [2]string{entry.Pattern, metric.Name}
+				if utils.FilterMatch(entry.Pattern, metric.Name) {
+					result = append(result, metric.Name)
+				}
 			}
 		}
 	}
-
-	close(itemChan)
-
-	itemWg.Wait()
-
-	result := set.StringSlice(itemSet)
-	sort.Strings(result)
 
 	return result
 }
