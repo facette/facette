facette/facette

commit df34d251bec37318707b99d5cf59ea4358325ef5

Do not build docs by default

Makefile
@@ -89,11 +89,13 @@ build-assets: node_modules
 		$(call mesg_ok) || $(call mesg_fail)
 
 build-docs:
+ifneq ($(filter build_docs,$(BUILD_TAGS)),)
 	@for man in $(MAN_LIST); do \
 		$(call mesg_start,docs,Generating $$man manual page...); \
 		install -d -m 0755 $(BUILD_DIR)/man && $(PANDOC) $(PANDOC_ARGS) docs/man/$$man.md >$(BUILD_DIR)/man/$$man && \
 			$(call mesg_ok) || $(call mesg_fail); \
 	done
+endif
 
 test: test-bin
 

debian/rules
@@ -15,4 +15,4 @@ override_dh_auto_test:
 endif
 
 override_dh_auto_install:
-	$(MAKE) PREFIX=$(CURDIR)/debian/tmp/usr install
+	$(MAKE) PREFIX="$(CURDIR)/debian/tmp/usr" BUILD_TAGS="build_docs" install

commit 89ec1be27562523288d1e91dc667377ccbc78106

Fix Makefile
Fixes bug in Makefile introduced by df34d251bec37318707b99d5cf59ea4358325ef5.

Makefile
@@ -123,9 +123,11 @@ install-assets: build-assets
 		$(call mesg_ok) || $(call mesg_fail)
 
 install-docs: build-docs
+ifneq ($(filter build_docs,$(BUILD_TAGS)),)
 	@$(call mesg_start,install,Installing manual pages...)
 	@install -d -m 0755 $(PREFIX)/share/man/man1 && cp -r $(BUILD_DIR)/man/* $(PREFIX)/share/man/man1 && \
 		$(call mesg_ok) || $(call mesg_fail)
+endif
 
 lint: lint-bin lint-assets
 