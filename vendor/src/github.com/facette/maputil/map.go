a8e1c7f53cf1f46e9a7e6c918db4b9c6d49f1deb
Vincent Batoufflet <vincent@batoufflet.info>
2017-10-29 06:23:00 +0000

    Update "maputil" vendored package

--- vendor/src/github.com/facette/maputil/map.go
+++ vendor/src/github.com/facette/maputil/map.go
@@ -86,7 +86,7 @@ func (m Map) GetMap(key string, fallback Map) (Map, error) {
 		}
 
 	default:
-		for k, v := range val.(map[interface{}]interface{}) {
+		for k, v := range val.(map[string]interface{}) {
 			result[fmt.Sprintf("%v", k)] = v
 		}
 	}
